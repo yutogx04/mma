f26b7a272def1c6c3c9ed1b478c178cc719dbfbd
yutogx04 <lehanine.faical@gmail.com>
2025-11-15 15:52:35 +0100

    my bad

--- node_modules/vitest/dist/chunks/plugin.d.DevON6kQ.d.ts
+++
@@ -1,9 +0,0 @@
-import { V as Vitest, T as TestProject, a as TestProjectConfiguration } from './reporters.d.BQ0wpUaj.js';
-
-interface VitestPluginContext {
-	vitest: Vitest;
-	project: TestProject;
-	injectTestProjects: (config: TestProjectConfiguration | TestProjectConfiguration[]) => Promise<TestProject[]>;
-}
-
-export type { VitestPluginContext as V };
--- node_modules/vitest/dist/environments.js
+++
@@ -1,3 +0,0 @@
-export { e as builtinEnvironments, p as populateGlobal } from './chunks/index.BfmpdV5p.js';
-import 'node:url';
-import 'node:console';
