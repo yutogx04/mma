62a9eae56e065e2c6a3e8b49891e0c803cfdfa8d
yutogx04 <lehanine.faical@gmail.com>
2025-11-16 10:51:04 +0100

    added a simple test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+/// <reference types="vitest/config" />
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +16,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  test: {},
 })
