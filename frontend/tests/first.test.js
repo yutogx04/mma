8160b44b7a5bf55f29c33acb1e4af56c5e3496d2
yutogx04 <lehanine.faical@gmail.com>
2025-11-16 11:01:36 +0100

    modified test

--- frontend/tests/first.test.js
+++ frontend/tests/first.test.js
@@ -1,15 +1,6 @@
-import { increment } from './helpers'
-
-describe('increment', () => {
-  test('increments the current number by 1', () => {
-    expect(increment(0, 10)).toBe(1)
-  })
-
-  test('does not increment the current number over the max', () => {
-    expect(increment(10, 10)).toBe(10)
-  })
-
-  test('has a default max of 10', () => {
-    expect(increment(10)).toBe(10)
+//this is here just because we need at least one test file for vitest to run
+describe('test', () => {
+  test('test', () => {
+    expect(true).toBe(true)
   })
 })
